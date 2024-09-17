87981e920dba3dc9bf31932ba946d47b368d26ea
RomanT <tomrobryuc@gmail.com>
2024-09-17 14:31:50 +0300

    add new PostViewModel

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,15 +1,56 @@
 package ru.netology.nmedia
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 
 class PostViewModel : ViewModel() {
+    private val empty = Post(
+        id = 0,
+        content = "",
+        title = "",
+        likeByMe = false,
+        subtitle = "",
+        likes = 0,
+        share = 0,
+        view = 0
+    )
+
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
     fun setLike(id: Long) {
         repository.setLike(id)
     }
     fun setShare(id: Long) {
         repository.setShare(id)
     }
+
+    fun removeById(id: Long) = repository.removeById(id)
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+
+        }
+
+        edited.value = empty
+    }
+    fun escape () {
+        edited.value = empty
+    }
+
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
 
 }
